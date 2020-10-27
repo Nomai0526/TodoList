23c0eaec2c25e431d5db0ef40f34143511c291fa
WindReiner <44196436+WindReiner@users.noreply.github.com>
2020-10-27 11:50:50 +0800

    Improve the interface

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+	// body...
+	var todobtn = document.getElementById('finishbtn');
+	todobtn.addEventListener("click",function() {
+		console.log("todobtn");
+	})
+}
+// document.ready(function (argument) {
+// 	console.log('124')
+// })
+// console.log("123");
