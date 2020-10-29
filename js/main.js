3a5a34609cce6937ab450919b0f377393aab8568
WindReiner <44196436+WindReiner@users.noreply.github.com>
2020-10-29 09:19:08 +0800

    finish html

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 window.onload = function () {
-		console.log(localStorage.getItem('testKey'));
-	// body...
-	var todobtn = document.getElementById('finishbtn');
-	var addItemBtn = document.getElementById('additem');
-	addItemBtn.addEventListener('click',function function_name(argument) {
-		// body...
-	})	
-	todobtn.addEventListener("click",function() {
-		let testobj = {
-			name:'name',
-			content:'content',
-			time:1,
-		}
-		console.log(testobj);
-		localStorage.setItem("testKey",testobj);
-		var o = localStorage.getItem('testKey');
-		console.log("todobtn");
-		console.log(o);
-	})
+	// 	console.log(localStorage.getItem('testKey'));
+	// // body...
+	// var todobtn = document.getElementById('finishbtn');
+	// var addItemBtn = document.getElementById('additem');
+	// addItemBtn.addEventListener('click',function function_name(argument) {
+	// 	// body...
+	// })	
+	// todobtn.addEventListener("click",function() {
+	// 	let testobj = {
+	// 		name:'name',
+	// 		content:'content',
+	// 		time:1,
+	// 	}
+	// 	console.log(testobj);
+	// 	localStorage.setItem("testKey",testobj);
+	// 	var o = localStorage.getItem('testKey');
+	// 	console.log("todobtn");
+	// 	console.log(o);
+	// })
 }
 // document.ready(function (argument) {
 // 	console.log('124')
