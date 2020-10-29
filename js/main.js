ea7eefaf51951d05eb1f12dedf0fa57c91d77041
WindReiner <44196436+WindReiner@users.noreply.github.com>
2020-10-29 08:31:31 +0800

    js update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 window.onload = function () {
+		console.log(localStorage.getItem('testKey'));
 	// body...
 	var todobtn = document.getElementById('finishbtn');
 	todobtn.addEventListener("click",function() {
+		let testobj = {
+			name:'name',
+			content:'content',
+			time:1,
+		}
+		console.log(testobj);
+		localStorage.setItem("testKey",testobj);
+		var o = localStorage.getItem('testKey');
 		console.log("todobtn");
+		console.log(o);
 	})
 }
 // document.ready(function (argument) {
