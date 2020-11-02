4427fd8dd0c8a538332e840a3bb1dbd304a2b79d
littlenomai <anarchy8239@gmail.com>
2020-11-02 13:06:31 +0800

    update js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-// class Item {
-// 	constructor(Date,){
+class Item {
+	constructor(date,title,content){
+		this.date = date;
+		this.title = title;
+		this.content = content;
+	}
+}
 
-// 	}
-// }
+
+
+function contentflash() {
+	const contentbody = document.getElementById('contentbody');
+	for (let i = 0; i < localStorage.length; i++) {
+			let key = localStorage.key(i);
+			let item = localStorage.getItem(key); 
+		}	
+}
 
 
 window.onload = function () {
+
+	let item = new Item(new Date(),'title1','content1');
+	localStorage.setItem(JSON.stringify(item));
+
 	// 	console.log(localStorage.getItem('testKey'));
 	// // body...
 	// var todobtn = document.getElementById('finishbtn');
@@ -27,14 +43,14 @@ window.onload = function () {
 	// })
 	// localStorage.clear();
 
-	let content = document.getElementById('maincontent');
+	let contentbody = document.getElementById('contentbody');
 	let time1 = new Date();
 	let itemtimes = [time1,time1];
 	let itemtitles = ['title1'];
 	let itemcontents = ['content1'];
 	for (let i = 0; i < itemtimes.length; i++) {
 		console.log(itemtimes[i].getDay());
-		content.innerHTML = content.innerHTML + '<div class="timestamp">'+itemtimes[i].getFullYear()+'年'+(itemtimes[i].getMonth()+1)+'月'+itemtimes[i].getDate()+'日</div>' 
+		contentbody.innerHTML = contentbody.innerHTML + '<div class="timestamp">'+itemtimes[i].getFullYear()+'年'+(itemtimes[i].getMonth()+1)+'月'+itemtimes[i].getDate()+'日</div>' 
 		console.log("added");
 	} 
 
@@ -50,7 +66,7 @@ window.onload = function () {
 	}); 
 	confirmBtn.addEventListener('click',function(){
 		document.getElementById('additemwindow').style.visibility = 'hidden';
-		content.innerHTML = content.innerHTML + '<div class="todoitem"><span class="todologo"><img src="../icon/callicon.png" class="todoicon"></span><span class="todocontent"><div class="todotitle">摸鱼</div><div class="todoinfo">摸好几只鱼</div><div class="todotime">1919.8.10</div></span><span class="todobtn"><div id="finishbtn">完成</div><div id="editbtn">删除</div></span></div>';
+		contentbody.innerHTML = contentbody.innerHTML + '<div class="todoitem"><span class="todologo"><img src="../icon/callicon.png" class="todoicon"></span><span class="todocontent"><div class="todotitle">摸鱼</div><div class="todoinfo">摸好几只鱼</div><div class="todotime">1919.8.10</div></span><span class="todobtn"><div id="finishbtn">完成</div><div id="editbtn">删除</div></span></div>';
 	}); 
 	cancalBtn.addEventListener('click',function(){
 		document.getElementById('additemwindow').style.visibility = 'hidden';
